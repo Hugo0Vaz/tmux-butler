ae2cebe084df9d6339ff2f74e8f412e515df9aa4
Hugo0Vaz <hugomartinsvaz@gmail.com>
2023-05-14 18:54:07 -0300

    implementando parsing de argumentos de linha de comando

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // session configuration file path
+    #[arg(short, long)]
+    config_path: String,
+
+    // flag to check if it is ran as a zsh post cmd hook
+    #[arg(short, long, default_value_t = false)]
+    zhook: bool,
+}
 fn main() {
-    println!("Hello, world!");
+
+    let args = Args::parse();
+
+    println!("{}", args.config_path);
+
+    if args.config_path.is_empty() {
+        println!("meu deus")
+    }
 }
